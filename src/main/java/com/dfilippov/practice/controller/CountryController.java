7cfb66a876f499a7ed792e5fba055e22e19f82d5
Daniil <penzadaniilfilippov99@mail.ru>
2022-06-22 09:06:19 +0300

    countries and docs requests

--- src/main/java/com/dfilippov/practice/controller/CountryController.java
+++ src/main/java/com/dfilippov/practice/controller/CountryController.java
@@ -1,4 +1,21 @@
 package com.dfilippov.practice.controller;
 
+import com.dfilippov.practice.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CountryController {
+    @Autowired
+    CountryService countryService;
+    @GetMapping("/countries")
+    private ResponseEntity getCountries(){
+        try{
+            return ResponseEntity.ok(countryService.getCountries());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
--- src/main/java/com/dfilippov/practice/controller/DocController.java
+++ src/main/java/com/dfilippov/practice/controller/DocController.java
@@ -1,4 +1,22 @@
 package com.dfilippov.practice.controller;
 
+import com.dfilippov.practice.service.DocService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DocController {
+    @Autowired
+    DocService docService;
+    @GetMapping("/docs")
+    public ResponseEntity getDocs(){
+        try{
+            return ResponseEntity.ok(docService.getDocs());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
 }
